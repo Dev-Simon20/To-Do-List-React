16029af9b182eab41da8f80aab99ec61fd74259b
Dev-Simon20 <jeanpierks6@gmail.com>
2023-09-29 22:57:48 -0500

    Version basica 1.0 terminada

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import './App.css'
 import Entrada from './Componentes/Entrada'
 import List from './Componentes/List'
 import { useState } from 'react'
+import {v4 as uuid} from'uuid'
 function App() {
   
    const [texto,setText]=useState([]); 
    
    const extraer=(text)=>{
-    const newText=[...texto,text]
-       setText(newText);
+     const textToObject={info:text,identifi:uuid()}
+    const newText=[...texto,textToObject]
+    setText(newText);
+
+    
+   }
+
+   const eliminar=(ident)=>{
+        const newText=texto.filter((valor)=>valor.identifi!=ident);
+         setText(newText)
    }
 
   return (
@@ -18,7 +27,7 @@ function App() {
        <Entrada extraer={extraer}/>
        <div className='container-task-list'>
         {/* El index es una propiedad del map y el key se tiene que pasar como parametro cuando se crean varios componentes de uno */}
-         {texto.map((t,index)=><List texto={t} key={index} />
+         {texto.map((t,index)=><List texto={t.info} key={index} id={t.identifi} eliminar={eliminar}/>
          )}
         </div>
        </div>
--- src/Componentes/List/index.jsx
+++ src/Componentes/List/index.jsx
@@ -1,12 +1,25 @@
 import './List.css'
+import { useState } from 'react';
 
-const List =({texto})=>{
+const List =(props)=>{
+   const {texto,id}=props;
+   const {eliminar}=props
+   const[color,setColor]=useState('white');
+
+   const cambiarColor=()=>{
+      if (color=='white') {
+         setColor('green')
+      }
+      else{
+        setColor('white')
+      }
+   }
     return (<>
       <div className='conten-tarea'>
-      <i className="fa-solid fa-trash"></i>
+      <i className="fa-solid fa-trash trash" id={id} onClick={(e)=>eliminar(e.target.id)}></i>
          <span className='conten-tarea-text'> {texto}
          </span>
-         <i className="fa-regular fa-circle-check"></i>
+         <i className="fa-regular fa-circle-check"  onClick={()=>cambiarColor()} style={{color:color}}></i>
       </div>
     
     </>)
